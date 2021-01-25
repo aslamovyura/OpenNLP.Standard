c29eb7b3a7a924bc992697e50fd3101704b572de
Yury Aslamov <yaslamov@caeonline.com>
2021-01-25 17:27:33 +0300

    Migrated to .Net Core v1.0

--- src/ConsoleTest/Program.cs
+++ src/ConsoleTest/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleTest
         {
             string str = ".NET Framework (pronounced dot net) is a software framework developed by Microsoft that runs primarily on Microsoft Windows. It includes a large class library known as Framework Class Library (FCL) and provides language interoperability (each language can use code written in other languages) across several programming languages. Programs written for .NET Framework execute in a software environment (as contrasted to hardware environment) known as Common Language Runtime (CLR), an application virtual machine that provides services such as security, memory management, and exception handling. (As such, computer code written using .NET Framework is called 'managed code'.) FCL and CLR together constitute .NET Framework.";
             var arr = new NLPClass().Split(str);
+
             foreach (var s in arr)
             {
                 Console.WriteLine(s);
